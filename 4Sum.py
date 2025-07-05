588320e974a5e69abcdd15d093c3f4679577ad45
Himesh Dhaka <127547250+himesh026@users.noreply.github.com>
2025-07-05 16:50:40 +0530

    Add 4Sum

--- 4Sum.py
+++ 4Sum.py
@@ -1,12 +1,4 @@
 # Problem: 4Sum
-# URL: https://leetcode.com/problems/4sum/description/
+# URL: https://leetcode.com/problems/4sum/submissions/1687142939/
 
-class Solution {
-public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-       vector<vector<int>> ans;
-       sort(nums.begin(),nums.end());
-       int n=nums.size();
-       for(int i=0;i<n;i++){
-        if(i>0 && nums[i]==nums[i-1]) continue;
-        for(int j=i+1;j<n;j++){
+c
