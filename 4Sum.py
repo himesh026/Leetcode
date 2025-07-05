e516c673860fa7c4f5c116da8ff08f806ba50a04
Himesh Dhaka <127547250+himesh026@users.noreply.github.com>
2025-07-05 16:51:21 +0530

    Add 4Sum

--- 4Sum.py
+++ 4Sum.py
@@ -1,4 +1,13 @@
 # Problem: 4Sum
-# URL: https://leetcode.com/problems/4sum/submissions/1687142939/
+# URL: https://leetcode.com/problems/4sum/submissions/1687143549/
 
-c
+                        right--;
+                    }
+                }
+            }
+        }
+       }
+      
+       return ans;
+    }
+};
