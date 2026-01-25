ee618a96d5577c0f5d795cdeba13f9d806d44075
Himesh Dhaka <127547250+himesh026@users.noreply.github.com>
2026-01-25 18:54:10 +0530

    Add Minimum Difference Between Highest and Lowest of K Scores

---
+++ Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores.cpp
@@ -0,0 +1,16 @@
+// Problem: Minimum Difference Between Highest and Lowest of K Scores
+// URL: https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/?envType=daily-question&envId=2026-01-25
+
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+
+        sort(nums.begin(),nums.end());
+        int ans=INT_MAX;
+        for(int i=0;i<=nums.size()-k;i++){
+            ans=min(ans,nums[i+k-1]-nums[i]);
+
+        }
+        return ans;
+    }
+};
